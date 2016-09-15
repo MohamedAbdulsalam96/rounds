216a427350020cf6809999d9196fa480af127d43
hemant <hemant@pema.co.za>
2016-09-15 20:32:22 +0200

    Rounds App completion

--- rounds/__init__.py
+++ rounds/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import frappe
+import frappe, json
 
 __version__ = '0.0.1'
 
 @frappe.whitelist(allow_guest=False)
 def reset_round_seen(name):
     round = frappe.get_doc("Rounds Chanted", name)
-    round.reset_seen
-    frappe.msgprint(round._seen)
+    return json.loads(round._seen)
