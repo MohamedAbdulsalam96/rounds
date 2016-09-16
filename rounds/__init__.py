30c8cfaa37d480c6f34bff55bd13dd82e1a8efb7
hemant <hemant@pema.co.za>
2016-09-16 07:36:00 +0200

    Rounds Completed with notification

--- rounds/__init__.py
+++ rounds/__init__.py
@@ -6,8 +6,11 @@ __version__ = '0.0.1'
 
 @frappe.whitelist(allow_guest=False)
 def reset_round_seen(name):
-    seenby = '["' + frappe.session.user + '"]'
-    sql = """UPDATE `tabRounds Chanted` set _seen = '%s' where name='%s'""" % (seenby,name)
-    #seenby.append(str(frappe.session.user))
-    round = frappe.db.sql(sql, as_dict=True)
-    return len(round)#sql
+    # seenby = '["' + frappe.session.user + '"]'
+    # sql = """UPDATE `tabRounds Chanted` set _seen = '%s' where name='%s'""" % (seenby,name)
+    # #seenby.append(str(frappe.session.user))
+    # round = frappe.db.sql(sql, as_dict=True)
+    # return len(round)#sql
+    round = frappe.get_doc("Rounds Chanted", name)
+    round.add_seen()
+    
