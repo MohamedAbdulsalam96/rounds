24921f84e2322939fa5babc5b1b45881049f2393
hemant <hemant@pema.co.za>
2016-09-16 13:09:41 +0200

    Balance for devotees Rounds

--- rounds/rounds/doctype/devotee/devotee.js
+++ rounds/rounds/doctype/devotee/devotee.js
@@ -3,6 +3,21 @@
 
 frappe.ui.form.on('Devotee', {
 	refresh: function(frm) {
-
+	},
+	target: function(frm){
+//	    console.log(frm.doc.target);
+//        user = frm.doc.user;
+//        frappe.call({
+//            method: "rounds.rounds.doctype.devotee.devotee.update_goal_date",
+//            args: {
+//                "user": user,
+//                "target": frm.doc.target
+//            },
+//            callback: function(r) {
+//                //frappe.model.sync(r.message);
+//                frm.refresh();
+//                //console.log(user);
+//            }
+//        })
 	}
 });
--- rounds/rounds/doctype/devotee/devotee.py
+++ rounds/rounds/doctype/devotee/devotee.py
@@ -15,4 +15,31 @@ class Devotee(Document):
 		#frappe.msgprint(user.first_name)
 		self.db_set('full_name', " ".join(filter(None, [user.first_name, user.middle_name, user.last_name])),
 					update_modified=False)
+		last = frappe.db.sql("""select closing_balance_names from `tabRounds Chanted`
+	    				where devotee=%s order by date DESC LIMIT 1""", (self.user), as_dict=False)
+		if len(last) > 0:
+			for d in last:
+				# devotee = devotee = frappe.get_doc('Devotee', frappe.get_value('Devotee',{'user':user},'name'))
+				# devotee.target = target
+				# frappe.msgprint(str(d[0]))
+				daily = self.daily_minimum_rounds*108*16
+				remaining = long(self.target)-long(d[0])
+				days = long(remaining)/long(daily)
+				# frappe.msgprint(str(remaining) + " " +str(daily) + " " + str(days))
+				self.reach_by = frappe.utils.add_days(frappe.utils.today(),days)
 
+
+@frappe.whitelist(allow_guest=False)
+def update_goal_date(user, target):
+	last = frappe.db.sql("""select closing_balance_names from `tabRounds Chanted`
+	    				where devotee=%s order by date DESC LIMIT 1""", (user), as_dict=True)
+	start_here = 1
+	if len(last) > 0:
+		for d in last:
+			devotee = devotee = frappe.get_doc('Devotee', frappe.get_value('Devotee',{'user':user},'name'))
+			devotee.target = target
+			daily = devotee.daily_minimum_rounds*108*16
+			days = long(target-d)/long(daily)
+			frappe.msgprint(str(target)+" " + str(daily) + " " + str(days))
+			devotee.reach_by = frappe.utils.add_days(frappe.utils.today(),days)
+			devotee.save()
