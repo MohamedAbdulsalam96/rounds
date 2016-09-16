bc9bf062092a3899248131312e8bcd8a0645fd5b
hemant <hemant@pema.co.za>
2016-09-16 16:00:37 +0200

    Balance for devotees Rounds

--- rounds/rounds/doctype/devotee/devotee.py
+++ rounds/rounds/doctype/devotee/devotee.py
@@ -15,18 +15,20 @@ class Devotee(Document):
 		#frappe.msgprint(user.first_name)
 		self.db_set('full_name', " ".join(filter(None, [user.first_name, user.middle_name, user.last_name])),
 					update_modified=False)
-		last = frappe.db.sql("""select closing_balance_names from `tabRounds Chanted`
+		last = frappe.db.sql("""select closing_balance_names, date from `tabRounds Chanted`
 	    				where devotee=%s order by date DESC LIMIT 1""", (self.user), as_dict=False)
 		if len(last) > 0:
 			for d in last:
 				# devotee = devotee = frappe.get_doc('Devotee', frappe.get_value('Devotee',{'user':user},'name'))
 				# devotee.target = target
-				# frappe.msgprint(str(d[0]))
+				# frappe.msgprint(str(d))
 				daily = self.daily_minimum_rounds*108*16
 				remaining = long(self.target)-long(d[0])
 				days = long(remaining)/long(daily)
 				# frappe.msgprint(str(remaining) + " " +str(daily) + " " + str(days))
 				self.reach_by = frappe.utils.add_days(frappe.utils.today(),days)
+				self.balance_as_at = d[1]
+				self.current_balance = d[0]
 
 
 @frappe.whitelist(allow_guest=False)
--- rounds/rounds/doctype/rounds_chanted/rounds_chanted.py
+++ rounds/rounds/doctype/rounds_chanted/rounds_chanted.py
@@ -37,12 +37,13 @@ class RoundsChanted(Document):
 @frappe.whitelist(allow_guest=False)
 def update_balance(user):
 	# frappe.msgprint(user)
-	first = frappe.db.sql("""select name from `tabRounds Chanted`
-    				where devotee=%s and start_here=0 order by date LIMIT 1""", (user), as_dict=True)
+	first = frappe.db.sql("""select name, _seen from `tabRounds Chanted`
+    				where devotee=%s and start_here=0 order by date LIMIT 1""", (user), as_dict=False)
 	start_here = 1
 	if len(first) > 0:
+		# frappe.msgprint(str(first))
 		for d in first:
-			round = frappe.get_doc('Rounds Chanted', d)
+			round = frappe.get_doc('Rounds Chanted', d[0])
 			if round:
 				round.back_log = round.minimum_number - round.total_chanted
 
@@ -54,7 +55,12 @@ def update_balance(user):
 				else:
 					round.closing_balance_chanted = round.openning_balance_chanted + round.back_log
 
+				round.start_here = start_here
 				round.closing_balance_names = round.openning_balance_names + round.total_names
+				# frappe.msgprint(str(d))
+				if d[1] !=None:
+					round.updated = True
+
 				round.save()
 
 				closing_chanted = round.closing_balance_chanted
@@ -76,6 +82,11 @@ def update_balance(user):
 								round.back_log = 0
 						else:
 							round.closing_balance_chanted = round.openning_balance_chanted + round.back_log
+
+						seen = frappe.db.sql("""select _seen from `tabRounds Chanted` where name=%s""", (round.name), as_dict=False)
+						# frappe.msgprint(str(seen))
+						if d[0] != None:
+							round.updated = True
 
 						round.closing_balance_names = round.openning_balance_names + round.total_names
 						round.start_here = start_here
