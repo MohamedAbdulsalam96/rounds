81e17d67822016568d2f4b81525cf64085d22cb5
hemant pema <hemant@pema.co.za>
2019-06-09 13:13:08 +0200

    hpema

---
+++ rounds/config/rounds.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+from frappe import _
+import frappe
+
+
+def get_data():
+    return [
+		{
+			"label": _("Rounds"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Rounds"
+				},
+			]
+		},
+		{
+			"label": _("Devotees"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Devotees"
+				},
+			]
+		},
+	]
